3ac22913a32faa993107d4fdd864139014b85f10
Fabio <fabioaz.guedes@hotmail.com>
2022-06-18 15:01:51 -0300

    fix deploy issue

---
+++ data/resourcesData.ts
@@ -0,0 +1,422 @@
+const resourcesData = [
+  {
+    heading: 'Ferramentas de desenvolvimento',
+    list: [
+      {
+        name: 'Visual Studio Code',
+        description:
+          'Editor de código otimizado para criar e depurar aplicativos modernos da Web e da nuvem',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426992/portfolio/Visual_Studio_Code_cuinds.png',
+        alt: 'Visual Studio Code',
+        link: 'https://visualstudio.microsoft.com/pt-br/',
+      },
+      {
+        name: 'Minhas extensões VSCode',
+        description:
+          'Veja exatamente quais são as extensões que eu estou usando no meu Visual Studio Code',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426992/portfolio/Visual_Studio_Code_Extensions_hkjohu.png',
+        alt: 'Extensões para Visual Studio Code',
+        link: 'https://medium.com/@fabioaz.guedes/minhas-extens%C3%B5es-no-vscode-como-desenvolvedor-frontend-aa85d40fab10',
+      },
+      {
+        name: 'Playcode',
+        description:
+          'Editor e compilador de Javascript totalmente online. Perfeito para quem quer experimentar a linguagem.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426991/portfolio/Playcode_l2az8t.png',
+        alt: 'Playcode',
+        link: 'https://playcode.io/',
+      },
+      {
+        name: 'Codepen',
+        description:
+          'CodePen é um ambiente de desenvolvimento social para designers e desenvolvedores front-end.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426990/portfolio/Codepen_bv3cvc.png',
+        alt: 'Codepen',
+        link: 'https://codepen.io/',
+      },
+    ],
+  },
+  {
+    heading: 'Tratar imagens',
+    list: [
+      {
+        name: 'Remove.bg',
+        description:
+          'Site cuja inteligência artificial permite remover o fundo de imanges, deixando-as transparente.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426991/portfolio/Remove.Bg_moihbt.png',
+        alt: 'Remove.bg',
+        link: 'https://www.remove.bg/pt-br',
+      },
+      {
+        name: 'Reduce Images',
+        description:
+          'Altera o tamanho e a qualidade da imagem, deixando-a mais leve e mais fácil de enviar para o site.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426991/portfolio/Reduce_Images_gkmekg.png',
+        alt: 'Reduce Images',
+        link: 'https://www.reduceimages.com/',
+      },
+      {
+        name: 'Tiny PNG',
+        description:
+          'Diminui seletivamente o número de cores da imagem, deixando a imagem mais leve e pronta para ser upada no site.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655428823/portfolio/Tiny_PNG_gjfwf8.png',
+        alt: 'Tiny PNG',
+        link: 'https://tinypng.com/',
+      },
+      {
+        name: 'Optimizilla',
+        description:
+          'Utiliza uma combinação inteligente dos melhores algoritmos de otimização e compressão para reduzir as imagens.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655428823/portfolio/Optimizilla_cavybk.png',
+        alt: 'Optimizilla',
+        link: 'https://imagecompressor.com/pt/',
+      },
+      {
+        name: 'Favicon.io',
+        description:
+          'Gere rapidamente seu favicon a partir de texto, imagem ou escolha entre centenas de emojis.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655428875/portfolio/Favicon.io_exzzcj.png',
+        alt: 'Favicon.io',
+        link: 'https://favicon.io/',
+      },
+    ],
+  },
+  {
+    heading: 'Análise de SEO',
+    list: [
+      {
+        name: 'Meta Tags',
+        description:
+          'Ferramente que gera automaticamente Meta Tags para qualquer site.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655428904/portfolio/Meta_Tags_y1bxbc.png',
+        alt: 'Meta Tags',
+        link: 'https://metatags.io/',
+      },
+      {
+        name: 'SEO Site Checkup',
+        description:
+          'Ferramente que percorre todo site em busca de erros e acertos no SEO, apontando também as possíveis melhorias.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426992/portfolio/SEO_Site_Checkup_yjm7lx.png',
+        alt: 'SEO Site Checkup',
+        link: 'https://seositecheckup.com/',
+      },
+    ],
+  },
+  {
+    heading: 'Ferramentas de Design',
+    list: [
+      {
+        name: 'Canva',
+        description:
+          'Plataforma de design gráfico que permite aos usuários criar os mais diversos tipos de conteúdos visuais.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426990/portfolio/Canva_ck1dxk.png',
+        alt: 'Canva',
+        link: 'https://www.canva.com/',
+      },
+      {
+        name: 'Figma',
+        description:
+          'Editor gráfico de vetor e prototipagem de projetos de design baseado principalmente no navegador web.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426990/portfolio/Figma_pia2de.png',
+        alt: 'Figma',
+        link: 'https://figma.com/',
+      },
+      {
+        name: 'Clean Mock',
+        description:
+          'Crie mockups impressionantes usando os quadros de dispositivos mais recentes com fundos personalizados.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426990/portfolio/Clean_Mock_gg66n2.png',
+        alt: 'Clean Mock',
+        link: 'https://www.cleanmock.com/',
+      },
+    ],
+  },
+  {
+    heading: 'Geradores de CSS',
+    list: [
+      {
+        name: 'Clippy',
+        description:
+          'Gera um código de clip-path em diferentes formas como elipses, polígonos, setas, estrelas, etc.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426990/portfolio/Clippy_z4iz0z.png',
+        alt: 'Clippy',
+        link: 'https://bennettfeely.com/clippy/',
+      },
+      {
+        name: 'CSS Gradient',
+        description:
+          'Gerador com pré-visualização e biblioteca de gradientes com cores harmônicas para CSS.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426992/portfolio/CSS_Gradient_v2eyoq.png',
+        alt: 'CSS Gradient',
+        link: 'https://cssgradient.io/',
+      },
+      {
+        name: 'Neumorphism.io',
+        description:
+          'Gerador de cores, gradientes e sombras que se adaptam ao novo conceito de design.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655514030/portfolio/neumorphism.io_zclwvh.png',
+        alt: 'CSS Gradient',
+        link: 'https://neumorphism.io/#e0e0e0',
+      },
+      {
+        name: 'Smooth Shadow',
+        description: 'Ferramenta para criar sombras complexas em CSS',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655514030/portfolio/Smooth_Shadow_iesyae.png',
+        alt: 'CSS Gradient',
+        link: 'https://shadows.brumm.af/',
+      },
+    ],
+  },
+  {
+    heading: 'Bibliotecas de Imagens',
+    list: [
+      {
+        name: 'Unsplash',
+        description:
+          'Imagens e fotos bonitas e gratuitas que você pode baixar e usar em qualquer projeto.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426992/portfolio/Unsplash_mjunrc.png',
+        alt: 'Unsplash',
+        link: 'https://unsplash.com/',
+      },
+      {
+        name: 'Pixabay',
+        description:
+          'Comunidade vibrante de criativos, compartilhando imagens e vídeos com licença gratuita de uso.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655428934/portfolio/pixabay_tddfsv.png',
+        alt: 'Pixabay',
+        link: 'https://pixabay.com/pt/',
+      },
+      {
+        name: 'Freepik',
+        description:
+          'Milhões de recursos gráficos grátis para seus projetos criativos.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426990/portfolio/Freepik_na7zt6.png',
+        alt: 'Freepik',
+        link: 'https://br.freepik.com/',
+      },
+      {
+        name: 'Humaaans',
+        description:
+          'Misture e combine ilustrações de pessoas com essa biblioteca de design.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426990/portfolio/Humaaans_sgmuif.png',
+        alt: 'Humaaans',
+        link: 'https://www.humaaans.com/',
+      },
+      {
+        name: 'unDraw',
+        description:
+          'Ilustrações de código aberto para qualquer ideia que você possa imaginar e criar.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426992/portfolio/UnDraw_duw73s.png',
+        alt: 'Undraw',
+        link: 'https://undraw.co/illustrations',
+      },
+      {
+        name: 'UI Faces',
+        description:
+          'Avatares gratuitos que você pode usar em seus modelos de design.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426991/portfolio/UI_Faces_en7ieu.png',
+        alt: 'UI Faces',
+        link: 'https://www.uifaces.co/',
+      },
+    ],
+  },
+  {
+    heading: 'Bibliotecas de Ícones',
+    list: [
+      {
+        name: 'UXWing',
+        description:
+          'Ícones gratuitos e bem otimizados que podem ser usados no design de interface Web e de aplicativos.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426992/portfolio/UXWing_teuz1k.png',
+        alt: 'UXWing',
+        link: 'https://uxwing.com/',
+      },
+      {
+        name: 'Icons8',
+        description:
+          'Ícones, ilustrações, fotos, músicas e ferramentas de design.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426992/portfolio/Icons8_pazequ.png',
+        alt: 'Icons8',
+        link: 'https://icons8.com.br/',
+      },
+      {
+        name: 'Flaticon',
+        description:
+          'Baixe ícones e adesivos gratuitos para os seus projetos. Recursos feitos por e para designers.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426990/portfolio/Flaticon_uiwrow.png',
+        alt: 'Flaticon',
+        link: 'https://www.flaticon.com/br/',
+      },
+      {
+        name: 'Google Fonts',
+        description:
+          'Ícones disponíveis em cinco estilos e uma variedade de tamanhos e densidades para download.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426990/portfolio/Google_Fonts_n1pu1q.png',
+        alt: 'Google Fonts',
+        link: 'https://fonts.google.com/icons',
+      },
+      {
+        name: 'HTML Codes',
+        description:
+          'Encontre facilmente símbolos, entidades, caracteres e códigos HTML com valores ASCII, HEX, CSS e Unicode.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426992/portfolio/HTML_Codes_rnpxgc.png',
+        alt: 'HTML Codes',
+        link: 'https://www.toptal.com/designers/htmlarrows/symbols/',
+      },
+    ],
+  },
+  {
+    heading: 'Extensões para Chrome',
+    list: [
+      {
+        name: 'Colorzilla',
+        description:
+          'Descobre a cor de qualquer elemento da página através de um conta-gotas avançado.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655572412/portfolio/colorzilla_zk8hkm.png',
+        alt: 'Colorzilla',
+        link: 'https://chrome.google.com/webstore/detail/colorzilla/bhlhnicpbhignbdhedgjhgdocnmhomnp?hl=pt',
+      },
+      {
+        name: 'FireShot',
+        description:
+          'Captura, edita e salva imagens completas da página da web.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655572574/portfolio/fireshot_voqjar.png',
+        alt: 'FireShot',
+        link: 'https://chrome.google.com/webstore/detail/take-webpage-screenshots/mcbpblocgmgfnpjjppndjkmgjaogfceg',
+      },
+      {
+        name: 'Lighthouse',
+        description:
+          'Audita o desempenho, a acessibilidade e a otimização do mecanismo de pesquisa de páginas da web. ',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655572412/portfolio/lighthouse_pfftho.png',
+        alt: 'Lighthouse',
+        link: 'https://chrome.google.com/webstore/detail/lighthouse/blipmdconlkpinefehnmjammfjpmpbjk?hl=pt-BR',
+      },
+      {
+        name: 'Site Palette',
+        description:
+          'Extrai as cores de qualquer página web e cria uma paleta personalizada.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655572412/portfolio/sitepalette_iw2ga4.png',
+        alt: 'Site Palette',
+        link: 'https://chrome.google.com/webstore/detail/site-palette/pekhihjiehdafocefoimckjpbkegknoh?hl=en-GB',
+      },
+      {
+        name: 'VisBug',
+        description:
+          'Possui ferramentas para alterar layouts de páginas da web e ajuda para fazer pequenas edições CSS.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655572412/portfolio/visbug_egekup.png',
+        alt: 'VisBug',
+        link: 'https://chrome.google.com/webstore/detail/visbug/cdockenadnadldjbbgcallicgledbeoc',
+      },
+      {
+        name: 'WhatFont',
+        description:
+          'A forma mais fácil de descobrir qual fonte um página web está usando.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655572412/portfolio/whatfont_n7x2fi.png',
+        alt: 'WhatFont',
+        link: 'https://chrome.google.com/webstore/detail/whatfont/jabopobgcpjmedljpbcaablpmlmfcogm?hl=pt-BR',
+      },
+    ],
+  },
+  {
+    heading: 'Para se Inspirar',
+    list: [
+      {
+        name: 'Land-book',
+        description:
+          'Galeria de web design com curadoria para criativos, atualizada diariamente.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426991/portfolio/Land-Book_nbquzh.png',
+        alt: 'Land-book',
+        link: 'https://land-book.com/',
+      },
+      {
+        name: 'Dribbble',
+        description:
+          'Milhões de designers e agências em todo o mundo exibem seus trabalhos de portfólio no Dribbble.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426990/portfolio/Dribbble_thlvnj.png',
+        alt: 'Dribbble',
+        link: 'https://dribbble.com/',
+      },
+      {
+        name: 'Startup Landing',
+        description:
+          'Grande coleção de landing pages absolutamente gratuitas construídas com React, Next JS e Gatsby JS.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426992/portfolio/Startup_Landing_vb2vwz.png',
+        alt: 'Startup Landing',
+        link: 'https://startuplanding.redq.io/',
+      },
+      {
+        name: 'Sketch App Sources',
+        description:
+          'A maior coleção de recursos gratuitos e premium para aplicativos da Web, dispositivos móveis, UI e UX.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655428969/portfolio/Sketch_App_Sources_juvtlu.png',
+        alt: 'Sketch App Sources',
+        link: 'https://www.sketchappsources.com/',
+      },
+      {
+        name: 'Color Hunt',
+        description:
+          'Obtenha inspiração de cores para seus projetos de design e arte.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655426992/portfolio/Color_Hunt_drygev.png',
+        alt: 'Color Hunt',
+        link: 'https://colorhunt.co/',
+      },
+    ],
+  },
+  {
+    heading: 'Para ir mais longe',
+    list: [
+      {
+        name: 'Medium',
+        description:
+          'Me siga no medium para ter acesso a uma coletânia atualizada de artigos sobre Javascript.',
+        image:
+          'https://res.cloudinary.com/fabioguedesj/image/upload/v1655510185/portfolio/medium_rihvgj.png',
+        alt: 'Medium',
+        link: 'https://medium.com/@fabioaz.guedes',
+      },
+    ],
+  },
+];
+
+export default resourcesData;
